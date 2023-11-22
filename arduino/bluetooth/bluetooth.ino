136a38b7d72055fb6b8a97fce81dc6d021be0e26
Just Koopen <just.koopen@hva.nl>
2023-11-22 16:43:57 +0100

    Code for sending BLE data added (?)

--- arduino/bluetooth/bluetooth.ino
+++ arduino/bluetooth/bluetooth.ino
@@ -1,27 +1,65 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-web-bluetooth/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
 BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
+BLECharacteristic* pSensorCharacteristic = NULL;
+BLECharacteristic* pLedCharacteristic = NULL;
 bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
 
-#define SERVICE_UUID        "9051282e-893f-11ee-b9d1-0242ac120002"
-#define CHARACTERISTIC_UUID "a73e2e56-893f-11ee-b9d1-0242ac120002"
+const int ledPin = 18; // Use the appropriate GPIO pin for your setup
 
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-  }
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
+#define SERVICE_UUID        "73770700-a4e0-4ff2-bd68-47a5250d5ec2"
+#define SENSOR_CHARACTERISTIC_UUID "544a3ce0-5ca6-411e-a0c2-17789dc0cec8"
+#define LED_CHARACTERISTIC_UUID "791ac637-4048-44c7-8ac9-bc1ae24aefc7"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pLedCharacteristic) {
+        std::string value = pLedCharacteristic->getValue();
+        if (value.length() > 0) {
+            Serial.print("Characteristic event, written: ");
+            Serial.println(static_cast<int>(value[0])); // Print the integer value
+
+            int receivedValue = static_cast<int>(value[0]);
+            if (receivedValue == 1) {
+                digitalWrite(ledPin, HIGH);
+            } else {
+                digitalWrite(ledPin, LOW);
+            }
+        }
+    }
 };
 
 void setup() {
   Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
 
   // Create the BLE Device
   BLEDevice::init("ESP32S3");
@@ -31,36 +69,65 @@ void setup() {
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 1); // use different value if necessary
+  BLEService *pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      BLEUUID(CHARACTERISTIC_UUID),
-                      BLECharacteristic::PROPERTY_READ |
+  pSensorCharacteristic = pService->createCharacteristic(
+                      SENSOR_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // Create the ON button Characteristic
+  pLedCharacteristic = pService->createCharacteristic(
+                      LED_CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_WRITE
                     );
 
-  // Add descriptor to the characteristic for notifications
-  pCharacteristic->addDescriptor(new BLE2902());
+  // Register the callback for the ON button characteristic
+  pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pSensorCharacteristic->addDescriptor(new BLE2902());
+  pLedCharacteristic->addDescriptor(new BLE2902());
 
   // Start the service
   pService->start();
 
   // Start advertising
-  pServer->getAdvertising()->addServiceUUID(BLEUUID(SERVICE_UUID));
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting for connections...");
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  if (deviceConnected) {
-    if (Serial.available()) {
-      String data = Serial.readStringUntil('\n');
-      pCharacteristic->setValue(data.c_str());
-      pCharacteristic->notify();
-      Serial.println("Data sent over BLE: " + data);
+    // notify changed value
+    if (deviceConnected) {
+        pSensorCharacteristic->setValue(String(value).c_str());
+        pSensorCharacteristic->notify();
+        value++;
+        Serial.print("New value notified: ");
+        Serial.println(value);
+        delay(3000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
-  }
-
-  delay(100);
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        Serial.println("Device disconnected.");
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("Start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+        Serial.println("Device Connected");
+    }
 }

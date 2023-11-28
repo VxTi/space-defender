9ca9857a534219360bfcb3428272d9a853a37386
Just Koopen <just.koopen@hva.nl>
2023-11-28 14:19:08 +0100

    Added select player functionality

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -91,6 +91,27 @@ void toggleDebugMode()
   }
 }
 
+char * selectPlayer(){
+  int playerNumber = 0;
+  debugPrint("Selecting player");
+  while(!playerNumber){
+    debugPrint("Waiting for input..");
+    if (digitalRead(PIN_BUTTON_LEFT)){
+      playerNumber = 1;
+      debugPrint("Player 1 selected");
+      break;
+    }
+    if (digitalRead(PIN_BUTTON_RIGHT)){
+      playerNumber = 2;
+      debugPrint("Player 2 selected");
+      break;
+    }
+  }
+  char * output = "ESP32 Controller P0";
+  output[strlen(output) - 1] = '0' + playerNumber;
+  return output;
+}
+
 // Checks if debug mode is toggled, and if so sends the input to serial
 void debugPrint(String text)
 {
@@ -131,7 +152,7 @@ void setup()
   analogReadResolution(12); // For reading from 0 to 4095
 
   // Create the BLE Device
-  BLEDevice::init(DEVICE_BT_NAME);
+  BLEDevice::init((std::__cxx11::string)selectPlayer());
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();

683b87660b2a3624bc975cd38125f215db4d8b55
Just Koopen <just.koopen@hva.nl>
2023-12-06 18:07:49 +0100

    Added database functionality to server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = 8080;
 
 const express = require('express');
 const { uuid } = require('uuidv5');
-const mysql = require('mysql2');
+const mssql = require("mysql");
 const app = express();
 const cors = require('cors');
 
@@ -22,20 +22,47 @@ let server = app.listen(port, `0.0.0.0`, () => {
     console.log(`Server has started on port ${server.address().address}:${port}`);
 
 });
-var con = mysql.createConnection({
-  host: "oege.ie.hva.nl",
-  user: "koopenj",
-  password: "pSmwQExG/1rux.",
-  database: "zkoopenj",
-  port: "3306",
 
-});
+// Get request
+app.get('/', function (req, res) {
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("INSERT INTO highscores (score) VALUES (10000)", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
+  // Config your database credential
+  const config = {
+    user: 'koopenj',
+    password: 'pSmwQExG/1rux.',
+    server: 'oege.ie.hva.nl',
+    database: 'zkoopenj'
+  };
+
+  // Connect to your database
+  const connection = mssql.createConnection(config);
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      return;
+    }
+
+    // Query to the database and get the records
+    connection.query('SELECT * FROM `highscores` WHERE tom = 1', function (err, records) {
+
+      if (err) {
+        console.error('Error querying database:', err);
+        connection.end(); // Close the database connection
+        return;
+      }
+
+      // Here, 'records' contains the data retrieved from the database
+      // You can use it in your code as needed
+      // For example, let's store it in a variable and log it
+      const retrievedData = records;
+      console.log(`Retrieved data: ${retrievedData}`);
+
+      // Close the database connection
+      connection.end();
+
+      // Send a success response
+      res.send('Data retrieved from the database and stored in a variable. Check the server console for the data.');
+    });
   });
 });
